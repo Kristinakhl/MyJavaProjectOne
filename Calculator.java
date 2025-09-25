011538d1c2d9d11ebc3bf90191042cf37914278f
CTPweb-pr <kristina.khlistunova@gmail.com>
2025-09-25 13:20:27 +0300

    Добавлен калькулятор двух чисел

--- Calculator.java
+++ Calculator.java
@@ -3,18 +3,18 @@ import java.util.Scanner;
 public class Calculator {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Введите первое число: ");
+        System.out.print("Enter first number: ");
         double a = sc.nextDouble();
-        System.out.print("Введите второе число: ");
+        System.out.print("Enter second number: ");
         double b = sc.nextDouble();
 
-        System.out.println("Сумма: " + (a + b));
-        System.out.println("Разность: " + (a - b));
-        System.out.println("Произведение: " + (a * b));
+        System.out.println("Sum: " + (a + b));
+        System.out.println("Difference: " + (a - b));
+        System.out.println("Product: " + (a * b));
         if (b != 0) {
-            System.out.println("Деление: " + (a / b));
+            System.out.println("Division: " + (a / b));
         } else {
-            System.out.println("Деление на ноль невозможно!");
+            System.out.println("Division by zero is impossible!");
         }
     }
 }
